69c40e0f720096896b6d4a60ef971f24e59c19ae
gawaooooo <gawao57@gmail.com>
2023-10-31 19:03:20 +0900

    refactor(type): 型を変更

--- my-app/src/app/_utils/hackerNews.ts
+++ my-app/src/app/_utils/hackerNews.ts
@@ -1,4 +1,4 @@
-export async function getTopStories(): Promise<Array<Number>> {
+export async function getTopStories(): Promise<Array<number>> {
   return fetch("https://hacker-news.firebaseio.com/v0/topstories.json", {
     next: { revalidate: 60 }, // 1分経過するとキャッシュを破棄する
   }).then((res) => {
@@ -11,24 +11,24 @@ type Item = {
   id: Number;
   deleted?: Boolean;
   type: "job" | "story" | "comment" | "poll" | "pollopt";
-  by: String;
-  time: Number;
-  text?: String;
-  dead?: Boolean;
-  parent?: Number;
-  poll?: Number;
-  kids?: Array<Number>;
-  url?: String;
-  score?: Number;
-  title?: String;
-  parts?: Array<Number>;
-  descendants?: Number;
+  by: string;
+  time: number;
+  text?: string;
+  dead?: boolean;
+  parent?: number;
+  poll?: number;
+  kids?: Array<number>;
+  url?: string;
+  score?: number;
+  title?: string;
+  parts?: Array<number>;
+  descendants?: number;
 };
 
 /**
  * 記事のデータを取得する
  */
-export async function getItem(id: Number): Promise<Item> {
+export async function getItem(id: number): Promise<Item> {
   return fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(
     (res) => {
       return res.json();
