311c3c70e13f1ea3f387cfcfc32ca33a27b8fcd3
gawaooooo <gawao57@gmail.com>
2023-10-31 19:25:57 +0900

    feat: 記事の詳細表示追加

--- my-app/src/app/top20/[id]/page.tsx
+++ my-app/src/app/top20/[id]/page.tsx
@@ -8,15 +8,33 @@ export default async function Top20IdPage({ params }: { params: Params }) {
   const { id } = params;
   const item = await getItem(id);
 
+  // 記事データ内に含まれるコメントIDを1件ずつ取得する
+  const kids = item.kids
+    ? await Promise.all(item.kids.map((kidsItem) => getItem(kidsItem)))
+    : [];
+
   return (
     <article>
-      <h1 className="text-xl">{item.title}</h1>
+      <h1 className="text-2xl">{item.title}</h1>
       <p className="py-2">
         by {item.by} on {new Date(item.time * 1000).toLocaleString()}
       </p>
-      <p>
-        <a href={item.url} className="underline">{item.url}</a>
+      <p className="pt-2 pb-4">
+        <a href={item.url} className="underline">
+          {item.url}
+        </a>
       </p>
+      <h2 className="text-xl">Comments</h2>
+      {kids.map((kidsItem) => (
+        <div
+          key={String(kidsItem.id)}
+          className="py-2 border-b-2 border-gray-400"
+        >
+          <h3 className="text-lg">by: {kidsItem.by}</h3>
+          <p className="py-2">{kidsItem.text}</p>
+          <p>{new Date(kidsItem.time * 1000).toLocaleString()}</p>
+        </div>
+      ))}
     </article>
   );
 }
